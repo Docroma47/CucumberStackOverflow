560ec1ddc283a0fea80a4d71ebadf269df0757c5
Vlad Kolotoff <vkolotoff@pm.me>
2020-04-22 14:46:29 +0100

    feat (task #32): adding json report for azure html reporter

--- src/test/java/org/docroma47/cucumberstackoverflow/CucumberRunnerTest.java
+++ src/test/java/org/docroma47/cucumberstackoverflow/CucumberRunnerTest.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
     features = {"src/test/java/org/docroma47/cucumberstackoverflow/feature"},
-    glue = {"org.docroma47.cucumberstackoverflow.steps"}
+    glue = {"org.docroma47.cucumberstackoverflow.steps"},
+    plugin = { "pretty", "json:./results/cucumber.json" }
 )
 public class CucumberRunnerTest {
 
