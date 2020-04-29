c504b09f754993fef990d510ca19304c4aefa045
Vlad Kolotoff <vkolotoff@pm.me>
2020-04-29 11:57:33 +0100

    feat (task #32): correctly closing driver after each feature run

--- src/test/java/org/docroma47/cucumberstackoverflow/config/CucumberConfiguration.java
+++ src/test/java/org/docroma47/cucumberstackoverflow/config/CucumberConfiguration.java
@@ -8,7 +8,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
+import static io.cucumber.spring.CucumberTestContext.SCOPE_CUCUMBER_GLUE;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 @EnableConfigurationProperties({ StackoverflowProperties.class })
@@ -17,6 +19,7 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 public class CucumberConfiguration {
 
   @Bean(destroyMethod = "quit")
+  @Scope(SCOPE_CUCUMBER_GLUE)
   public WebDriver webDriver() {
     if ((System.getProperty("os.name").substring(0, 3)).equals("Lin")) {
       System.setProperty("webdriver.gecko.driver", "Drivers/Linux/geckodriver");
