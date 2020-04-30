bbdcbbfbe3f9c6f5e8dc7ddd5e076949cfee1d83
Roman <mom47@yandex.ru>
2020-04-30 23:01:20 +0300

    Added: FindBy.

--- src/test/java/org/docroma47/cucumberstackoverflow/page/UserPreferencesPage.java
+++ src/test/java/org/docroma47/cucumberstackoverflow/page/UserPreferencesPage.java
@@ -1,5 +1,4 @@
 package org.docroma47.cucumberstackoverflow.page;
-import java.util.Map;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -11,52 +10,53 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import static io.cucumber.spring.CucumberTestContext.SCOPE_CUCUMBER_GLUE;
-import static java.util.Map.entry;
-import static java.util.Map.ofEntries;
 
 @Component
 @Scope(SCOPE_CUCUMBER_GLUE)
 public class UserPreferencesPage extends AbstractPage {
 
-  @FindBy(id = "left-sidebar")
-  private WebElement leftSidebar;
-
-  private Map<String, String> uiElements = ofEntries(
-      entry("Preference", "//a[text()='preferences']"),
-      entry("Edit-profile", "//a[text()='Edit profile and settings']"),
-      entry("Profile", "//a[@class='my-profile js-gps-track']"),
-      entry("Dark-theme", "//input[@id='enableForcedDarkmode']"),
-      entry("Light-theme", "//input[@id='enableForcedLightmode']"),
-      entry("Keyboard-shortcuts", "//input[@id='keyboardShortcuts']"),
-      entry("Top-bar-xpath", "/html/body/header"),
-      entry("Top-bar-stickiness", "//*[@id='fixedHeader']"),
-      entry("Start-download-button", "//button[text()='Start download']"),
-      entry("Real-name", "//*[@id='RealName']"),
-      entry("hide-Hot-network-questions", "//input[@id='hotNetworkQuestions']"),
-      entry("Hot-network-questions", "//*[@id='sidebar']//*[@id='hot-network-questions']"),
-      entry("hide-Left-navigation", "//*[@id='content']//*[@id='flag-leftnav']"),
-      entry("Left-navigation-panel", "//*[@id='left-sidebar']")
-  );
-
-  private By getUiElement(String key) {
-    return By.xpath(uiElements.get(key));
-  }
-
-  private void navigateToPage(By xpath) {
-    driver.findElement(xpath).click();
-  }
+  @FindBy(xpath = "//*[@id='left-sidebar']")
+  private WebElement leftNavigationPanel;
+  @FindBy(xpath = "//*[@id='content']//*[@id='flag-leftnav']")
+  private WebElement hideLeftNavigationPanel;
+  @FindBy(xpath = "//*[@id='sidebar']//*[@id='hot-network-questions']")
+  private WebElement hotNetwordkQuestions;
+  @FindBy(xpath = "//input[@id='hotNetworkQuestions']")
+  private WebElement hideHotNetwordkQuestions;
+  @FindBy(xpath = "//*[@id='RealName']")
+  private WebElement realName;
+  @FindBy(xpath = "//button[text()='Start download']")
+  private WebElement startDownloadButton;
+  @FindBy(xpath = "//*[@id='fixedHeader']")
+  private WebElement topBarStickness;
+  @FindBy(xpath = "/html/body/header")
+  private WebElement topBar;
+  @FindBy(xpath = "//input[@id='keyboardShortcuts']")
+  private WebElement keyboardShortcats;
+  @FindBy(xpath = "//input[@id='enableForcedLightmode']")
+  private WebElement lightTheme;
+  @FindBy(xpath = "//input[@id='enableForcedDarkmode']")
+  private WebElement darkTheme;
+  @FindBy(xpath = "//a[@class='my-profile js-gps-track']")
+  private WebElement profile;
+  @FindBy(xpath = "//a[text()='Edit profile and settings']")
+  private WebElement editProfile;
+  @FindBy(xpath = "//a[text()='preferences']")
+  private WebElement preference;
+  @FindBy(xpath = "//body")
+  private WebElement body;
 
   public void navigateToPreferences() {
-    driver.findElement(getUiElement("Preference")).click();
+    preference.click();
   }
 
   public String getPageColor() {
-    return driver.findElement(By.xpath("//body")).getCssValue("background-color");
+    return body.getCssValue("background-color");
   }
 
   public void navigateToProfile() {
-    navigateToPage(getUiElement("Profile"));
-    navigateToPage(getUiElement("Edit-profile"));
+    profile.click();
+    editProfile.click();
   }
 
   public void inputHotKeysGAndH() {
@@ -66,58 +66,57 @@ public class UserPreferencesPage extends AbstractPage {
   }
 
   public void setKeyboardShortcuts(boolean enabled) {
-    WebElement keyboard = driver.findElement(getUiElement("Keyboard-shortcuts"));
-    if (!keyboard.isSelected() && enabled || keyboard.isSelected() && !enabled) {
-      keyboard.click();
+    if (!keyboardShortcats.isSelected() && enabled || keyboardShortcats.isSelected() && !enabled) {
+      keyboardShortcats.click();
     }
   }
 
   public void setTopBarStickiness(boolean enabled) {
-    WebElement topBarStickiness = driver.findElement(getUiElement("Top-bar-stickiness"));
-    if (!topBarStickiness.isSelected() && enabled || topBarStickiness.isSelected() && !enabled) {
-      topBarStickiness.click();
+    if (!topBarStickness.isSelected() && enabled || topBarStickness.isSelected() && !enabled) {
+      topBarStickness.click();
     }
   }
 
   public void setTheme(String theme) {
-    WebElement setTheme = driver.findElement(getUiElement(theme));
-    setTheme.click();
+    if (theme.equals("Dark-theme")) {
+      darkTheme.click();
+    } else if (theme.equals("Light-theme")){
+      lightTheme.click();
+    }
   }
 
   public Boolean isTopBarFixed() {
-    return driver.findElement(getUiElement("Top-bar-xpath")).getAttribute("class").contains("fixed");
+    return topBar.getAttribute("class").contains("fixed");
   }
 
   public void setHideHotNetworkQuestions(boolean enabled) {
-    WebElement keyboard = driver.findElement(getUiElement("hide-Hot-network-questions"));
-    if (!keyboard.isSelected() && enabled || keyboard.isSelected() && !enabled) {
-      keyboard.click();
+    if (!hideHotNetwordkQuestions.isSelected() && enabled || hideHotNetwordkQuestions.isSelected() && !enabled) {
+      hideHotNetwordkQuestions.click();
     }
   }
 
   public boolean isHotNetworkQuestionsDisplayed() {
-    if (driver.findElements(getUiElement("Hot-network-questions")).isEmpty()) {
+    if (driver.findElements(By.xpath("//*[@id='sidebar']//*[@id='hot-network-questions']")).isEmpty()) {
       return false;
     } else {
-      return driver.findElement(getUiElement("Hot-network-questions")).isDisplayed();
+      return hotNetwordkQuestions.isDisplayed();
     }
   }
 
   public String getRealName() {
     WebDriverWait wait = new WebDriverWait(driver, 2);
-    wait.until(ExpectedConditions.visibilityOf(driver.findElement(getUiElement("Real-name"))));
-    return driver.findElement(getUiElement("Real-name")).getAttribute("value");
+    wait.until(ExpectedConditions.visibilityOf(realName));
+    return realName.getAttribute("value");
   }
 
   public void setHideLeftNavigation(boolean enabled) {
-    WebElement hideLeftNavigation = driver.findElement(getUiElement("hide-Left-navigation"));
-    if (!hideLeftNavigation.isSelected() && enabled || hideLeftNavigation.isSelected() && !enabled) {
-      hideLeftNavigation.click();
+    if (!hideLeftNavigationPanel.isSelected() && enabled || hideLeftNavigationPanel.isSelected() && !enabled) {
+      hideLeftNavigationPanel.click();
     }
   }
 
   public Boolean isLeftNavigationPanelDisplayed() {
-    return leftSidebar.isDisplayed();
+    return leftNavigationPanel.isDisplayed();
   }
 
 }
