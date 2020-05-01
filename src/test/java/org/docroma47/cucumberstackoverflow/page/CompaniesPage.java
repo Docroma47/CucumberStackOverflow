25e801f43c2ed6bb0c314b89c94e79209bf94349
Docroma47 <mom47@yandex.ru>
2020-05-01 12:49:11 +0300

    Feature/task 73 find by for companies page (#26)
    * Added: FindBy for companies page.

--- src/test/java/org/docroma47/cucumberstackoverflow/page/CompaniesPage.java
+++ src/test/java/org/docroma47/cucumberstackoverflow/page/CompaniesPage.java
@@ -1,48 +1,44 @@
 package org.docroma47.cucumberstackoverflow.page;
-import java.util.Map;
 
 import org.docroma47.cucumberstackoverflow.config.StackoverflowProperties;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import static io.cucumber.spring.CucumberTestContext.SCOPE_CUCUMBER_GLUE;
-import static java.util.Map.of;
 
 @Component
 @Scope(SCOPE_CUCUMBER_GLUE)
-public class CompaniesPage {
+public class CompaniesPage extends AbstractPage {
 
   @Autowired
   private WebDriver driver;
   @Autowired
   private StackoverflowProperties properties;
 
-  private Map<String, String> uiElements = of(
-      "Jobs", "//*[@id='nav-jobs']",
-      "Companies", "//*[@id='TabCompanies']",
-      "Search-Field", "//*[@id='content']//*[@id='q']",
-      "Location-Filter-Field", "//*[@id='content']//*[@id='l']",
-      "Breadcrumb", "//*[@id='content']//*[@id='TabCompanies']"
-  );
-
-  private By getUiElement(String key) {
-    return By.xpath(uiElements.get(key));
-  }
+  @FindBy(id = "nav-jobs")
+  private WebElement jobsLink;
+  @FindBy(id = "TabCompanies")
+  private WebElement companiesBreadcrumb;
+  @FindBy(id = "q")
+  private WebElement searchField;
+  @FindBy(id = "l")
+  private WebElement locationFilterField;
 
   public void navigateToCompanies() {
-    driver.findElement(getUiElement("Jobs")).click();
-    driver.findElement(getUiElement("Companies")).click();
+    jobsLink.click();
+    companiesBreadcrumb.click();
   }
 
   public boolean isSearchFieldDisplayed() {
-    return driver.findElement(getUiElement("Search-Field")).isDisplayed();
+    return searchField.isDisplayed();
   }
 
   public boolean isLocationFilterFieldDisplayed() {
-    return driver.findElement(getUiElement("Location-Filter-Field")).isDisplayed();
+    return locationFilterField.isDisplayed();
   }
 
   public String getUrl() {
@@ -50,6 +46,6 @@ public class CompaniesPage {
   }
 
   public boolean isBreadcrumbSelected() {
-    return driver.findElement(getUiElement("Breadcrumb")).getAttribute("class").contains("is-selected");
+    return companiesBreadcrumb.getAttribute("class").contains("is-selected");
   }
 }
