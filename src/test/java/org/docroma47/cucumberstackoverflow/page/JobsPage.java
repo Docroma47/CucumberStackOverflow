1a8ad38d2de388113a29cd49cec51faf2166d422
Roman <mom47@yandex.ru>
2020-05-01 13:29:11 +0300

    Fix: garbage deleted.

--- src/test/java/org/docroma47/cucumberstackoverflow/page/JobsPage.java
+++ src/test/java/org/docroma47/cucumberstackoverflow/page/JobsPage.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -66,20 +64,14 @@ public class JobsPage extends AbstractPage {
 
   public void clickSearchButton() {
     searchButton.click();
-    WebDriverWait wait = new WebDriverWait(driver, 5, 10000);
-    wait.until(ExpectedConditions.visibilityOf(jobsLink));
   }
 
   public void selectJob(String index) {
-    WebDriverWait wait = new WebDriverWait(driver, 5, 10000);
-    wait.until(ExpectedConditions.visibilityOf(jobsLink));
     List<WebElement> listAd = driver.findElements(By.xpath("//*[@id='content']//*[@class='listResults']/div"));
     jobId = listAd.get(Integer.valueOf(index)).getAttribute("data-jobid");
   }
 
   public void saveSelectedJobAdd() {
-    WebDriverWait wait = new WebDriverWait(driver, 5, 10000);
-    wait.until(ExpectedConditions.visibilityOf(jobsLink));
     By xpath = By.xpath("//a[@data-jobid='" + getJobID() + "']");
     if (driver.findElement(xpath).getText().equals("Save")) {
       driver.findElement(xpath).click();
@@ -87,8 +79,6 @@ public class JobsPage extends AbstractPage {
   }
 
   public void clickSelectedJobAd() {
-    WebDriverWait wait = new WebDriverWait(driver, 5, 10000);
-    wait.until(ExpectedConditions.visibilityOf(jobsLink));
     String path;
     path = "//*[@class='listResults']//*[@data-result-id='" + getJobID() + "']" + "//a[@class='s-link stretched-link']";
     driver.findElement(By.xpath(path)).click();
