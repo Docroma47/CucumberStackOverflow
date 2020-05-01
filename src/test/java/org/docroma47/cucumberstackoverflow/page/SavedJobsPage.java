09d1ad29b9f6178a97927f8966ff67135d40905e
mom47@yandex.ru <79213980538r>
2020-05-02 00:24:08 +0300

    Fix: findBy for saved jobs page.

--- src/test/java/org/docroma47/cucumberstackoverflow/page/SavedJobsPage.java
+++ src/test/java/org/docroma47/cucumberstackoverflow/page/SavedJobsPage.java
@@ -1,43 +1,35 @@
 package org.docroma47.cucumberstackoverflow.page;
-import java.util.Map;
 
 import org.docroma47.cucumberstackoverflow.config.StackoverflowProperties;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.FindBy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import static io.cucumber.spring.CucumberTestContext.SCOPE_CUCUMBER_GLUE;
-import static java.util.Map.of;
 
 @Component
 @Scope(SCOPE_CUCUMBER_GLUE)
-public class SavedJobsPage {
+public class SavedJobsPage extends AbstractPage {
 
   @Autowired
   private WebDriver driver;
   @Autowired
   private StackoverflowProperties properties;
 
-  private Map<String, String> uiElements = of(
-      "Jobs", "//*[@id='nav-jobs']",
-      "SavedJobs", "//*[@id='TabSavedJobs']",
-      "Svg-icon", "//*[@id='content']//*[@class='svg-icon native ']",
-      "Button-browse-jobs", "//*[@id='content']//*[@class='s-btn s-btn__filled']",
-      "Breadcrumb", "//*[@id='content']//*[@id='TabSavedJobs']"
-  );
-
-  private By getUiElement(String key) {
-    return By.xpath(uiElements.get(key));
-  }
+  @FindBy(id = "nav-jobs")
+  private WebElement jobsLink;
+  @FindBy(id = "TabSavedJobs")
+  private WebElement savedJobsBreadcrumb;
+  @FindBy(xpath = "//*[@id='content']//*[@class='s-btn s-btn__filled']")
+  private WebElement browseJobsButton;
 
   public void navigateToSavedJobs() {
-    driver.findElement(getUiElement("Jobs")).click();
-    driver.findElement(getUiElement("SavedJobs")).click();
+    jobsLink.click();
+    savedJobsBreadcrumb.click();
   }
 
   public String getUrl() {
@@ -45,7 +37,7 @@ public class SavedJobsPage {
   }
 
   public boolean isBreadcrumbSelected() {
-    return driver.findElement(getUiElement("Breadcrumb")).getAttribute("class").contains("is-selected");
+    return savedJobsBreadcrumb.getAttribute("class").contains("is-selected");
   }
 
   public void deleteJob(String jobID) {
@@ -55,8 +47,6 @@ public class SavedJobsPage {
     }
     driver.findElement(By.xpath("//a[@data-jobid=" + jobID + "]")).click();
     driver.navigate().refresh();
-    WebDriverWait wait = new WebDriverWait(driver, 5, 10000);
-    wait.until(ExpectedConditions.visibilityOf(driver.findElement(getUiElement("Jobs"))));
   }
 
   public String findJob(String jobID) {
