0e93e0fe197bdaa188412d0b27a9292202aec389
Roman <mom47@yandex.ru>
2020-05-01 13:17:37 +0300

    Added: FindBy for messages page.

--- src/test/java/org/docroma47/cucumberstackoverflow/page/MessagesPage.java
+++ src/test/java/org/docroma47/cucumberstackoverflow/page/MessagesPage.java
@@ -1,47 +1,38 @@
 package org.docroma47.cucumberstackoverflow.page;
-import java.util.Map;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import static io.cucumber.spring.CucumberTestContext.SCOPE_CUCUMBER_GLUE;
-import static java.util.Map.of;
 
 @Component
 @Scope(SCOPE_CUCUMBER_GLUE)
-public class MessagesPage {
+public class MessagesPage extends AbstractPage {
 
   @Autowired
   private WebDriver driver;
 
-  private Map<String, String> uiElements = of(
-      "Jobs", "//*[@id='nav-jobs']",
-      "Messages", "//*[@id='TabMessages']",
-      "Title", "//*[@id='content']//*[contains (@class, 'fs-headline1')]",
-      "Breadcrumb", "//*[@id='content']//*[@id='TabMessages']"
-  );
-
-  public MessagesPage(WebDriver driver) {
-    this.driver = driver;
-  }
-
-  private By getUiElement(String key) {
-    return By.xpath(uiElements.get(key));
-  }
+  @FindBy(id = "nav-jobs")
+  private WebElement jobsLink;
+  @FindBy(id = "TabMessages")
+  private WebElement messagesBreadcrumb;
+  @FindBy(xpath = "//*[@id='content']//*[contains (@class, 'fs-headline1')]")
+  private WebElement title;
 
   public String getTitle() {
-    return driver.findElement(getUiElement("Title")).getText();
+    return title.getText();
   }
 
   public void navigateToMessages() {
-    driver.findElement(getUiElement("Jobs")).click();
-    driver.findElement(getUiElement("Messages")).click();
+    jobsLink.click();
+    messagesBreadcrumb.click();
   }
 
   public boolean isBreadcrumbSelected() {
-    return driver.findElement(getUiElement("Breadcrumb")).getAttribute("class").contains("is-selected");
+    return messagesBreadcrumb.getAttribute("class").contains("is-selected");
   }
 }
