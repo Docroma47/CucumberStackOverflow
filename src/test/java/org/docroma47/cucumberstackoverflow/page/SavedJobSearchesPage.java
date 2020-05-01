13886e319427b9dfb491bad3c02634cc39aabfb1
mom47@yandex.ru <79213980538r>
2020-05-02 00:21:08 +0300

    Added: extends AbstractPage.

--- src/test/java/org/docroma47/cucumberstackoverflow/page/SavedJobSearchesPage.java
+++ src/test/java/org/docroma47/cucumberstackoverflow/page/SavedJobSearchesPage.java
@@ -11,7 +11,7 @@ import static io.cucumber.spring.CucumberTestContext.SCOPE_CUCUMBER_GLUE;
 
 @Component
 @Scope(SCOPE_CUCUMBER_GLUE)
-public class SavedJobSearchesPage {
+public class SavedJobSearchesPage extends AbstractPage {
 
   @Autowired
   private WebDriver driver;
