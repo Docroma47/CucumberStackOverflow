1c06c165339210a64cdd3b10f545f22ad40f6118
Docroma47 <mom47@yandex.ru>
2020-05-03 13:21:19 +0300

    Feature/task 83 find by for tags page (#35)
    * Fix: findBy for tags page.

--- src/test/java/org/docroma47/cucumberstackoverflow/page/TagsPage.java
+++ src/test/java/org/docroma47/cucumberstackoverflow/page/TagsPage.java
@@ -1,36 +1,31 @@
 package org.docroma47.cucumberstackoverflow.page;
-import java.util.Map;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import static io.cucumber.spring.CucumberTestContext.SCOPE_CUCUMBER_GLUE;
-import static java.util.Map.of;
 
 @Component
 @Scope(SCOPE_CUCUMBER_GLUE)
-public class TagsPage {
+public class TagsPage extends AbstractPage {
 
   @Autowired
   private WebDriver driver;
 
-  private Map<String, String> uiElements = of(
-      "Tags", "//*[@id='nav-tags']",
-      "Title", "//*[@id='content']//*[contains (@class, 'fs-headline1')]"
-  );
-
-  private By getUiElement(String key) {
-    return By.xpath(uiElements.get(key));
-  }
+  @FindBy(id = "nav-tags")
+  private WebElement tagsLink;
+  @FindBy(xpath = "//*[@id='content']//*[contains (@class, 'fs-headline1')]")
+  private WebElement title;
 
   public String getTitle() {
-    return driver.findElement(getUiElement("Title")).getText();
+    return title.getText();
   }
 
   public void navigateToTags() {
-    driver.findElement(getUiElement("Tags")).click();
+    tagsLink.click();
   }
 }
